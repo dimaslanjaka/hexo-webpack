92fa8763aaef1232c93f07b1786c3c4d451c8507
dimaslanjaka <dimaslanjaka@gmail.com>
2023-10-06 14:36:31 +0700

    import main scss

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import './style.scss';
+import '@assets/css/main.scss';
 
 class Layout extends React.Component {
   render() {
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,5 @@ import ReactDOM from 'react-dom';
 //
 
 import App from './App';
-// import './assets/css/main.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
