ea0db8cffbfedb18650eaa2ef93f10e7c5b880f5
dimaslanjaka <dimaslanjaka@gmail.com>
2023-10-08 14:09:21 +0700

    add `fromRoot` path from root project

--- test/utils/index.js
+++ test/utils/index.js
@@ -1,8 +1,9 @@
 const path = require('upath');
-
+const cpaths = require('../../config/paths');
+const fromRoot = (...paths) => path.join(cpaths.cwd, ...paths);
 const tmp = (...paths) => path.join(__dirname, '/../tmp', ...paths);
 const fixtures = (...paths) => path.join(__dirname, '/../fixtures', ...paths);
 
-module.exports = { tmp, fixtures };
+module.exports = { tmp, fixtures, fromRoot };
 module.exports.tmp = tmp;
 module.exports.fixtures = fixtures;
