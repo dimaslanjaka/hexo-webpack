c697823d70aeed44a97b5b3325c13569ffe2b6a2
dimaslanjaka <dimaslanjaka@gmail.com>
2023-10-08 13:17:48 +0700

    add mixed html and markdown test

--- test/test-scripts/mixed.ts
+++ test/test-scripts/mixed.ts
@@ -1,18 +1,18 @@
 import { writefile } from 'sbg-utility';
-import prettierFormat from './format';
-import genRoute from './genRoute';
-import render from './render';
-import toJsx from './toJsx';
-import { fixtures, tmp } from './utils';
+import prettierFormat from '../format';
+import genRoute from '../genRoute';
+import render from '../render';
+import toJsx from '../toJsx';
+import { fixtures, tmp } from '../utils';
 
 render
   .init()
   .then(async () => {
-    const source = fixtures('jsx-failed.md');
+    const source = fixtures('mixed.md');
     const result = await genRoute(source);
     const route = writefile(__dirname + '/../routes.json', JSON.stringify([result], null, 2));
     const html = await prettierFormat(result.body, { parser: 'html' });
-    const jsx = await toJsx({ body: result.body, dest: tmp('jsx-failed'), source });
+    const jsx = await toJsx({ body: result.body, dest: tmp('mixed'), source });
     const wh = writefile(__dirname + '/tmp/body.html', html);
     console.log({ jsx: jsx.jsxPath, html: wh.file, route: route.file });
   })
