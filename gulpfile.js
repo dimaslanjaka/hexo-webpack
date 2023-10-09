b407cbcb0a6e799ca13edf716f68d02870c30b76
dimaslanjaka <dimaslanjaka@gmail.com>
2023-10-09 11:45:18 +0700

    add route generator task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
+require('ts-node').register({ compilerOptions: require('./tsconfig.json').compilerOptions });
 const { spawnAsync } = require('git-command-helper');
 const gulp = require('gulp');
+const { init } = require('./test/render');
+const { obj } = require('through2');
+const { fs, path, writefile } = require('sbg-utility');
+const { default: toJsx } = require('./test/toJsx');
+const paths = require('./config/paths');
+const { default: genRoute } = require('./test/genRoute');
+const Promise = require('bluebird');
 
 gulp.task('watch', function () {
   /**
@@ -13,4 +21,114 @@ gulp.task('watch', function () {
   run(() =>
     gulp.watch(['src/**/*.*', 'public/**/*.*', 'test/**/*.*', '!**/node_modules/**', '!**/dist/**', '!**/tmp/**'], run)
   );
+});
+
+// generate route stream method
+// clone of ./test/generate-route.js
+async function genR(options = {}) {
+  await init();
+  const dest = paths.src + '/posts';
+  if (options.clean) {
+    await fs.emptyDir(dest);
+  }
+  // let total = 0;
+  const routes = [];
+  const posts = require('./.cache/posts.json');
+  await Promise.all(posts).each(async postPath => {
+    /**
+     * @type {Promise<import('hexo-post-parser').Nullable<{ route: Awaited<ReturnType<typeof genRoute>>; jsx: Awaited<ReturnType<typeof toJsx>>; value: Record<string, any>; }>>}
+     */
+    const route = await genRoute(postPath);
+    try {
+      const jsx = await toJsx({
+        body: route.body,
+        source: route.source,
+        dest: path.join(dest, route.id)
+      });
+      // total++;
+      const value = { ...route, jsxPath: jsx.jsxPath };
+      // console.log(total, route.permalink);
+      routes.push(value);
+      writefile(__dirname + '/routes.json', JSON.stringify(routes, null, 2));
+      return { route, jsx, value };
+    } catch (e) {
+      console.error('jsx cannot parse', route.source);
+    }
+  });
+}
+
+// generate route from processed post
+// using `sbg post copy`
+gulp.task('rc', () => genR({ clean: true }));
+gulp.task('route', gulp.series(genR));
+gulp.task('r', gulp.series(genR));
+
+// mapping
+const yaml = require('yaml');
+const config = yaml.parse(fs.readFileSync(__dirname + '/_config.yml', 'utf-8'));
+
+gulp.task('map', function () {
+  const dest = __dirname + '/.cache/posts.json';
+  const routes = [];
+  const gc = require('./gulp-cache').default;
+  const cache = new gc();
+  return gulp
+    .src('**/*.md', {
+      cwd: __dirname + '/source/_posts',
+      ignore: [
+        '**/LICENSE',
+        '**/License.md',
+        '**/node_modules/**',
+        '**/readme.md',
+        '**/bin',
+        '**/.vscode',
+        '**/.frontmatter',
+        // add exclude from _config.yml
+        ...config.exclude
+      ]
+    })
+    .pipe(cache.start())
+    .pipe(
+      obj((vinyl, _enc, callback) => {
+        if (vinyl.isNull() || vinyl.isStream() || vinyl.isDirectory()) return callback(); // skip null and stream object
+        routes.push(vinyl.path);
+        // skip writing
+        callback();
+      })
+    )
+    .pipe(gulp.dest('./tmp/fake'))
+    .on('end', () => {
+      const w = writefile(dest, JSON.stringify(routes));
+      cache.onexit();
+      console.log('map stream ends', w.file);
+    });
+});
+
+// just testing
+gulp.task('direct', () => genR(__dirname + '/src-posts', { clean: true }));
+gulp.task('c', () => {
+  return gulp
+    .src('**/*.md', {
+      cwd: __dirname + '/src-posts',
+      ignore: [
+        '**/LICENSE',
+        '**/License.md',
+        '**/node_modules/**',
+        '**/readme.md',
+        '**/bin',
+        '**/.vscode',
+        '**/.frontmatter',
+        // add exclude from _config.yml
+        ...config.exclude
+      ]
+    })
+    .pipe(require('./gulp-cache').gulpCache())
+    .pipe(
+      obj((vinyl, _enc, callback) => {
+        if (vinyl.isNull() || vinyl.isStream() || vinyl.isDirectory()) return callback(); // skip null and stream object
+        //
+        callback(null, vinyl);
+      })
+    )
+    .pipe(gulp.dest(__dirname + '/tmp/fake-c'));
 });
