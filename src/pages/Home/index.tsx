cd8958e6554215262ba520fa2f0c85050fd6bef0
dimaslanjaka <dimaslanjaka@gmail.com>
2023-10-09 11:44:56 +0700

    add homepage

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,26 @@
+/// <reference path='../../types/image-jpg.d.ts' />
+
+import React from 'react';
+import logo from './logo.jpg';
+import { routeConfig } from '@project';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="mb-2">
+          <img src={logo} alt="" style={{ width: '40px' }} />
+        </div>
+        <u>
+          {routeConfig.map(o => (
+            <li key={o.title}>
+              <a href={o.permalink.replace(/.html$/, '')}>{o.permalink}</a>
+            </li>
+          ))}
+        </u>
+      </>
+    );
+  }
+}
+
+export default Home;
