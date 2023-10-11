bb9d1ce0c81beed47a3190f6bcad6246abef275f
dimaslanjaka <dimaslanjaka@gmail.com>
2023-10-11 16:55:36 +0700

    update flowbite UX layout

--- src/components/FlowbiteLayout/index.tsx
+++ src/components/FlowbiteLayout/index.tsx
@@ -1,11 +1,71 @@
 import React from 'react';
+import { BiBuoy } from 'react-icons/bi';
+import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from 'react-icons/hi';
 import { Outlet } from 'react-router-dom';
+import { SidebarProvider } from './context/SidebarContext';
+import Header from './header';
+import Sidebar from './sidebar';
+import './style.scss';
+import FlowbiteContext from './context/FlowbitContext';
 
 function FlowbiteLayout() {
   return (
-    <>
-      <Outlet />
-    </>
+    <FlowbiteContext>
+      <SidebarProvider>
+        <Header />
+        <div className="flex dark:bg-gray-900">
+          <main className="order-2 mx-4 mt-4 mb-24 flex-[1_0_16rem]">
+            <Outlet />
+          </main>
+          <div className="order-1">
+            <ActualSidebar />
+          </div>
+        </div>
+      </SidebarProvider>
+    </FlowbiteContext>
+  );
+}
+
+function ActualSidebar(): JSX.Element {
+  return (
+    <Sidebar>
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Sidebar.Item href="#" icon={HiChartPie}>
+            Dashboard
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiViewBoards}>
+            Kanban
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiInbox}>
+            Inbox
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiUser}>
+            Users
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiShoppingBag}>
+            Products
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiArrowSmRight}>
+            Sign In
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiTable}>
+            Sign Up
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+        <Sidebar.ItemGroup>
+          <Sidebar.Item href="#" icon={HiChartPie}>
+            Upgrade to Pro
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiViewBoards}>
+            Documentation
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={BiBuoy}>
+            Help
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
   );
 }
 
