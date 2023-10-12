bd4e2aee1aba42fabd585c7e9c9b4f5086a366a5
dimaslanjaka <dimaslanjaka@gmail.com>
2023-10-12 16:20:09 +0700

    separate build script

--- test/test-scripts/jsx-failed-2.ts
+++ test/test-scripts/jsx-failed-2.ts
@@ -1,19 +1,7 @@
-import { writefile } from 'sbg-utility';
-import prettierFormat from '../format';
-import genRoute from '../genRoute';
 import render from '../render';
-import toJsx from '../toJsx';
-import { fixtures, fromRoot, tmp } from '../utils';
+import build from './build';
 
 render
   .init()
-  .then(async () => {
-    const source = fixtures('jsx-failed2.md');
-    const result = await genRoute(source);
-    const route = writefile(fromRoot('routes.json'), JSON.stringify([result], null, 2));
-    const html = await prettierFormat(result.body, { parser: 'html' });
-    const jsx = await toJsx({ body: result.body, dest: tmp('jsx-failed2'), source });
-    const wh = writefile(tmp('body.html'), html);
-    console.log({ jsx: jsx.jsxPath, html: wh.file, route: route.file });
-  })
+  .then(() => build('jsx-failed2'))
   .catch(console.error);
