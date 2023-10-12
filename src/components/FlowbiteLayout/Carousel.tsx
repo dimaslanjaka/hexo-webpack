352ca4551ed49d8bfa7b739c106fde2332aac507
dimaslanjaka <dimaslanjaka@gmail.com>
2023-10-12 22:01:31 +0700

    fixed missing key prop

--- src/components/FlowbiteLayout/Carousel.tsx
+++ src/components/FlowbiteLayout/Carousel.tsx
@@ -9,10 +9,10 @@ interface Props {
 export default function FlowbiteCarousel(props: Props) {
   return (
     <Carousel>
-      {props.data.map(strObj => {
+      {props.data.map((strObj, i) => {
         const alt = typeof strObj === 'object' ? strObj.alt : strObj;
         const src = typeof strObj === 'object' ? strObj.src : strObj;
-        return <img alt={alt} src={src} height={String(props.height)} />;
+        return <img alt={alt} src={src} height={String(props.height)} key={alt + src + i} />;
       })}
     </Carousel>
   );
