2cf12c0c2f208015070aa7210efc2604fe3002c9
dimaslanjaka <dimaslanjaka@gmail.com>
2023-10-16 01:34:20 +0700

    add jsx options and overload types - true: return jsx element array - false: return script html string array

--- html/getDistScripts.tsx
+++ html/getDistScripts.tsx
@@ -4,13 +4,32 @@ import React from 'react';
 
 /**
  * get bundled webpack js from dist/index.html
+ * @returns return jsx element array
+ */
+function getDistScripts(): JSX.Element[];
+/**
+ * get bundled webpack js from dist/index.html
+ * @param jsx
+ * @returns return jsx element array
+ */
+function getDistScripts(jsx: true): JSX.Element[];
+/**
+ * get bundled webpack js from dist/index.html
+ * @param jsx
+ * @returns return script html string array
+ */
+function getDistScripts(jsx: false): string[];
+
+/**
+ * get bundled webpack js from dist/index.html
+ * @param jsx return jsx element
  * @returns
  */
-export default function getDistScripts() {
+function getDistScripts(jsx = true) {
   const generatedIndex = paths.build + '/index.html';
   const regex = /<script[\s\S]*?>[\s\S]*?<\/script>/gi;
   let m: RegExpExecArray | null = null;
-  const scripts = [] as JSX.Element[];
+  const scripts = [] as (JSX.Element | string)[];
 
   if (fs.existsSync(generatedIndex)) {
     const contents = fs.readFileSync(generatedIndex, 'utf-8');
@@ -23,10 +42,16 @@ export default function getDistScripts() {
         const srcRegEx = /src=["'](.*?)["']/g;
         const source = srcRegEx.exec(m[0]);
         if (source) {
-          scripts.push(<script src={source[1]} key={source[1]} defer={true}></script>);
+          if (jsx) {
+            scripts.push(<script src={source[1]} key={source[1]} defer={true}></script>);
+          } else {
+            scripts.push(`<script defer src="${source[1]}"></script>`);
+          }
         }
       }
     }
   }
   return scripts;
 }
+
+export default getDistScripts;
