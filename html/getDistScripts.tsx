59d4980f02aba0db8105d1559e4795bad9b5f5b3
dimaslanjaka <dimaslanjaka@gmail.com>
2023-10-16 01:40:39 +0700

    add return clean html

--- html/getDistScripts.tsx
+++ html/getDistScripts.tsx
@@ -2,23 +2,39 @@ import { fs } from 'sbg-utility';
 import paths from '../config/paths';
 import React from 'react';
 
+interface ReturnJSX {
+  scripts: JSX.Element[];
+  /**
+   * clean html string
+   */
+  html: string;
+}
+
+interface ReturnString {
+  scripts: JSX.Element[];
+  /**
+   * clean html string
+   */
+  html: string;
+}
+
 /**
  * get bundled webpack js from dist/index.html
  * @returns return jsx element array
  */
-function getDistScripts(): JSX.Element[];
+function getDistScripts(): ReturnJSX;
 /**
  * get bundled webpack js from dist/index.html
  * @param jsx
  * @returns return jsx element array
  */
-function getDistScripts(jsx: true): JSX.Element[];
+function getDistScripts(jsx: true): ReturnJSX;
 /**
  * get bundled webpack js from dist/index.html
  * @param jsx
  * @returns return script html string array
  */
-function getDistScripts(jsx: false): string[];
+function getDistScripts(jsx: false): ReturnString;
 
 /**
  * get bundled webpack js from dist/index.html
@@ -30,11 +46,12 @@ function getDistScripts(jsx = true) {
   const regex = /<script[\s\S]*?>[\s\S]*?<\/script>/gi;
   let m: RegExpExecArray | null = null;
   const scripts = [] as (JSX.Element | string)[];
+  let html = '';
 
   if (fs.existsSync(generatedIndex)) {
-    const contents = fs.readFileSync(generatedIndex, 'utf-8');
+    html = fs.readFileSync(generatedIndex, 'utf-8');
     // get script bundles
-    while ((m = regex.exec(contents)) !== null) {
+    while ((m = regex.exec(html)) !== null) {
       if (m.index === regex.lastIndex) {
         regex.lastIndex++;
       }
@@ -48,10 +65,14 @@ function getDistScripts(jsx = true) {
             scripts.push(`<script defer src="${source[1]}"></script>`);
           }
         }
+
+        // clean script tags from html
+        html = html.replace(m[0], '');
       }
     }
   }
-  return scripts;
+
+  return { scripts, html };
 }
 
 export default getDistScripts;
