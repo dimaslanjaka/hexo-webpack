5594c331e415cf248140e57684d4709df34a0084
Dimas Lanjaka <dimaslanjaka@gmail.com>
2023-10-16 07:19:43 +0700

    add header comment

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+// enable typescript file import from js file
 require('ts-node').register({
   compilerOptions: { ...require('./tsconfig.json').compilerOptions, checkJs: false, strict: false, noEmit: true },
   transpileOnly: true
